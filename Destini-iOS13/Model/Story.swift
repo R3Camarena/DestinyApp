b8b084cdc75c2a5cc0390ff8e0fc9d38ba8828ce
Ricardo Camarena <ricardocamarena@Ricardos-MacBook-Pro.local>
2022-07-19 23:59:09 -0500

    “DestinyApp”

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Story.swift
+//  DestinyApp
+//
+//  Created by Ricardo Camarena Colón on 19/07/2022.
+//  Copyright © 2022 Ricardo Camarena. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
